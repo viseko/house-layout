ce5219dfe6c1eaa1477473558bf3177a11342080
Viktor Konovalov <viseko23@gmail.com>
2024-05-20 10:48:53 +0700

    Added sticky header

--- src/widgets/page/header/index.js
+++ src/widgets/page/header/index.js
@@ -1,11 +1,25 @@
 import App from "../../../app/App.js";
 
 App.install(".js-header", (header) => {
+  let scrolled = false;
+
   function calcHeaderHeight() {
     const headerHeight = header.offsetHeight;
     document.body.style.setProperty("--header-height", `${headerHeight}px`);
   }
 
+  function scrollWatch() {
+    if (window.scrollY > 10) {
+      scrolled = true;
+      header.classList.add("_scrolled");
+    } else {
+      scrolled = false;
+      header.classList.remove("_scrolled");
+    }
+  }
+
   calcHeaderHeight();
+  scrollWatch();
   window.addEventListener("resize", calcHeaderHeight);
+  window.addEventListener("scroll", scrollWatch);
 });
